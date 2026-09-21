8ab8a86885feaedd88b117b40823f17c29023d50
holadstore <holadipofemi@gmail.com>
2026-09-21 19:32:56 +0100

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='y5q-public-v5';
+const ASSETS=['./','./index.html','./styles.css','./content.js','./app.js','./manifest.json','./about.html','./parents.html','./privacy.html','./terms.html','./404.html'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;}).catch(()=>caches.match('./index.html'))));});
